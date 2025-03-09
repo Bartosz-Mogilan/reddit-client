6f2e8e42237976b854fd5d789ed159e63943f015
Bartosz Mogilan <bmogilan@gmail.com>
2025-03-09 19:40:52 +0000

    updating post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,15 +1,19 @@
 import React from "react";
 import styles from "../css/post.module.css";
 
-function Post ({title, author, score, thumbnail, onClick}) {
+function Post({ title, author, score, thumbnail, onClick }) {
     return (
         <div className={styles.card} onClick={onClick}>
-            {thumbnail && <img src ={thumbnail} alt="Post thumbnail" className={styles.thumbnail} />}
-            <h3>{title}</h3>
-            <p>by {author}</p>
-            <p> Score: {score}</p>
+            {thumbnail && thumbnail.startWith("http") && (
+                <img src={thumbnail} alt="Post thumbnail" className={styles.thumbnail} />
+            )}
+            <div className={styles.content}>
+                <h3 className={styles.postTitle}>{title}</h3>
+                <p className={styles.postAuthor}>by {author}</p>
+                <p className={styles.postScore}>Score: {score}</p>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Post;
